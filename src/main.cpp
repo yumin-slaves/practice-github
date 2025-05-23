97cd26c50bc9b4b8aab49426f09d281a6cb859e0
yumin <sym2596@naver.com>
2025-05-23 14:20:13 +0900

    chore: 문제 TODO 정리

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,22 @@
 
 using namespace std;
 
-// TODO 0: dev/(your_name) 브랜치를 만들고, 다시 "feat/practice" 브랜치 생성하기
-
-// TODO 1. add.h에 정의된 add를 수정하여 제네릭을 활용하는 add() 함수로 만들고
-// 매개변수로 int, double을 매개변수로 넣어보고 commit 하기
-
-// TODO 2. C스타일에서 C++스타일로 리팩토링하고 commit 하기
-
-// TODO 3. dev/(your_name)에 feat/practice를 병합하기
-
-// TODO 4. Pull Request 해보기
+/* TODO 리스트
+*
+* TODO 1: dev/(your_name) 브랜치 생성
+* TODO 2. add.h에 정의된 add를 수정하여 템플릿을 활용하는 add() 함수로 만들고
+  매개변수로 int, double을 매개변수로 넣어보고 commit 하기
+* TODO 3. C스타일에서 C++스타일로 리팩토링하고 commit 하기
+* TODO 4. dev/(your_name)에 feat/practice를 병합하기
+* TODO 5. Pull Request 해보기
+*/
 
 int main() {
     std::cout << "Hello Linux!\n" << std::endl;
 
     double x = 3.14, y = 2.5;
+    
+    cout << "예상 결과값: 5.64\n코드 실행 결과: ";
     cout << add(x, y) << endl;
     
     cout << endl;
--- src/person.cpp
+++ src/person.cpp
@@ -2,7 +2,6 @@
 
 #include "person.h"
 
-
 using namespace std;
 
 int comp(const void *a, const void *b) {
@@ -13,23 +12,22 @@ int comp(const void *a, const void *b) {
 
 void func() {
     // 포인터 배열을 사용해야 할까?
-    Person* people = new Person[3] {
+    Person* people = new Person[4] {
         Person("서유민", 22, "PM"),
         Person("김민준", 21, "노예 1"),
-        Person("김선우", 21, "신입")
+        Person("김선우", 20, "신입 1"),
+        Person("박주영", 20, "신입 2")
     };
 
     // C 스타일의 반복문 보다 간단한게 있지 않을까?
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         people[i].print_info();
     }
     cout << endl;
 
     // C++에 정의된 sort 함수를 써보자
     
-    // for (int i = 0; i < 3; i++) {
-    //     people[i].print_info();
-    // }
+    // 출력하기
 
     delete[] people;
 }
